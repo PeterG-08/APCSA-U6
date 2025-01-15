65986c4a865fc20a1ffe13773621f876a4e4fdd2
Jack <jackn25@nycstudents.net>
2025-01-15 14:31:42 -0500

    finishing l2

---
+++ src/L2/Main.java
@@ -0,0 +1,11 @@
+package L2;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] names = {"Jamal", "Emily", "Destiny", "Mateo", "Sofia"};
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
