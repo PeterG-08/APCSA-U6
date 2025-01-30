a47c87da26ed4d2a16c9bc9182e2178d49e607ba
Jack <jackn25@nycstudents.net>
2025-01-30 12:56:39 -0500

    did l3-

---
+++ src/L3/LoopTest.java
@@ -0,0 +1,23 @@
+package L3;
+
+public class LoopTest
+{
+    public static void main(String[] args)
+    {
+        int[ ] values = {6, 2, 1, 7, 12, 5};
+
+        // triple each element in the array
+        for (int i = 0; i < values.length; i++)
+        {
+            values[i] *= 3;
+        }
+
+        // TEST!!!  Print out array to see if they changed
+        System.out.println("Array after the loop: ");
+
+        for (int val : values)
+        {
+            System.out.print(val + " ");
+        }
+    }
+}
